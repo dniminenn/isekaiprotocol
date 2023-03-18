43db5f1a4d2fdd84468142e9c70722a0bdefab99
dniminenn <85250163+dniminenn@users.noreply.github.com>
2023-03-18 20:09:41 -0300

    rewarder

--- contracts/stakingpools/waifurewarder.sol
+++ contracts/stakingpools/waifurewarder.sol
@@ -1,86 +1,95 @@
-// SPDX-License-Identifier: unlicense
-// THIS CODE IS DANGEROUS
+// SPDX-License-Identifier: unlicensed
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
-contract WaifuRewarder is ERC1155Holder, ReentrancyGuard, Ownable {
-    using SafeMath for uint256;
-
-    IERC20 public rewardToken;
+contract WaifuRewarder is Ownable, ReentrancyGuard {
     IERC1155 public nftToken;
-    uint256 public totalStaked;
-    mapping(address => mapping(uint256 => uint256)) public stakedBalances;
-    mapping(uint256 => address[]) public stakers;
-    mapping(uint256 => uint256) public stakedTokensTotal;
-    mapping(uint256 => uint256) public rewardRates;
-    mapping(address => mapping(uint256 => uint256)) public lastClaimed;
+    IERC20 public rewardToken;
 
-    event Staked(address indexed user, uint256 tokenId, uint256 amount);
-    event Unstaked(address indexed user, uint256 tokenId, uint256 amount);
-    event RewardClaimed(address indexed user, uint256 tokenId, uint256 rewardAmount);
+    uint256[] public rewardRates;
+    address[] public stakers;
 
-    constructor(IERC20 _rewardToken, IERC1155 _nftToken) {
-        rewardToken = _rewardToken;
+    mapping(address => mapping(uint256 => uint256)) public stakedTokens;
+    mapping(address => uint256) public pendingRewards;
+
+    event TokensStaked(address indexed user, uint256 tokenId, uint256 amount);
+    event TokensUnstaked(address indexed user, uint256 tokenId, uint256 amount);
+    event RewardClaimed(address indexed user, uint256 amount);
+
+    constructor(IERC1155 _nftToken, IERC20 _rewardToken, uint256[] memory _rewardRates) {
         nftToken = _nftToken;
+        rewardToken = _rewardToken;
+        rewardRates = _rewardRates;
     }
 
-    function setRewardRate(uint256 tokenId, uint256 rate) external onlyOwner {
-        rewardRates[tokenId] = rate;
+    modifier onlyRewardToken() {
+        require(msg.sender == address(rewardToken), "Caller is not the reward token");
+        _;
     }
 
-    function stake(uint256 tokenId, uint256 amount) external nonReentrant {
-        require(rewardRates[tokenId] > 0, "Reward rate not set for this token ID");
+    function totalStakedTokens() public view returns (uint256) {
+        uint256 total = 0;
+        for (uint256 i = 0; i < rewardRates.length; i++) {
+            total += rewardRates[i] * nftToken.totalSupply(i);
+        }
+        return total;
+    }
+
+        function stakeTokens(uint256 tokenId, uint256 amount) external nonReentrant {
+        require(amount > 0, "Cannot stake zero tokens");
+        require(tokenId < rewardRates.length, "Invalid token ID");
+
         nftToken.safeTransferFrom(msg.sender, address(this), tokenId, amount, "");
 
-        if (stakedBalances[msg.sender][tokenId] == 0) {
-            // Add user to the list of stakers for this token ID
-            stakers[tokenId].push(msg.sender);
-        }
+        stakedTokens[msg.sender][tokenId] += amount;
+        stakers.push(msg.sender);
 
-        stakedBalances[msg.sender][tokenId] = stakedBalances[msg.sender][tokenId].add(amount);
-        stakedTokensTotal[tokenId] = stakedTokensTotal[tokenId].add(amount);
-        totalStaked = totalStaked.add(amount);
-        emit Staked(msg.sender, tokenId, amount);
+        emit TokensStaked(msg.sender, tokenId, amount);
     }
 
-    function unstake(uint256 tokenId, uint256 amount) external nonReentrant {
-        require(rewardRates[tokenId] > 0, "Reward rate not set for this token ID");
-        require(stakedBalances[msg.sender][tokenId] >= amount, "Not enough staked tokens");
-
-        // Claim any pending rewards before unstaking
-        claimReward(tokenId);
+    function unstakeTokens(uint256 tokenId, uint256 amount) external nonReentrant {
+        require(amount > 0, "Cannot unstake zero tokens");
+        require(tokenId < rewardRates.length, "Invalid token ID");
+        require(stakedTokens[msg.sender][tokenId] >= amount, "Not enough staked tokens");
 
         nftToken.safeTransferFrom(address(this), msg.sender, tokenId, amount, "");
 
-        stakedBalances[msg.sender][tokenId] = stakedBalances[msg.sender][tokenId].sub(amount);
-        stakedTokensTotal[tokenId] = stakedTokensTotal[tokenId].sub(amount);
-        totalStaked = totalStaked.sub(amount);
-        emit Unstaked(msg.sender, tokenId, amount);
+        stakedTokens[msg.sender][tokenId] -= amount;
+
+        emit TokensUnstaked(msg.sender, tokenId, amount);
     }
 
-    function claimReward(uint256 tokenId) public nonReentrant {
-        require(rewardRates[tokenId] > 0, "Reward rate not set for this token ID");
-        uint256 stakedAmount = stakedBalances[msg.sender][tokenId];
-        require(stakedAmount > 0, "No staked tokens");
+    function distributeReward(uint256 rewardAmount) external onlyRewardToken {
+        uint256 totalTokens = totalStakedTokens();
+        require(totalTokens > 0, "No staked tokens");
 
-        uint256 lastClaimedTimestamp = lastClaimed[msg.sender][tokenId];
-        uint256 elapsedTime = block.timestamp.sub(lastClaimedTimestamp);
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address staker = stakers[i];
+            uint256 userTotalStakedTokens = 0;
 
-        uint256 pendingReward = stakedAmount.mul(rewardRates[tokenId]).mul(elapsedTime).div(86400); // 86400 seconds in a day
+            for (uint256 tokenId = 0; tokenId < rewardRates.length; tokenId++) {
+                userTotalStakedTokens += stakedTokens[staker][tokenId] * rewardRates[tokenId];
+            }
 
-        require(pendingReward > 0, "No rewards available");
-        require(rewardToken.balanceOf(address(this)) >= pendingReward, "Not enough reward tokens");
+            if (userTotalStakedTokens > 0) {
+                uint256 reward = (rewardAmount * userTotalStakedTokens) / totalTokens;
+                pendingRewards[staker] += reward;
+            }
+        }
+    }
 
-        rewardToken.transfer(msg.sender, pendingReward);
-        lastClaimed[msg.sender][tokenId] = block.timestamp;
+    function claimReward() external nonReentrant {
+        uint256 reward = pendingRewards[msg.sender];
+        require(reward > 0, "No pending rewards");
 
-        emit RewardClaimed(msg.sender, tokenId, pendingReward);
+        pendingRewards[msg.sender] = 0;
+
+        rewardToken.transfer(msg.sender, reward);
+
+        emit RewardClaimed(msg.sender, reward);
     }
 }
