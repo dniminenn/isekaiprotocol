a7d9df50595360b00df6ddc42672fd17075d6e21
dniminenn <j@lntgn.ca>
2023-03-19 23:04:29 -0300

    rework staking contract, this is seasonal variant

--- contracts/stakingpools/waifurewarder.sol
+++ contracts/stakingpools/waifurewarder.sol
@@ -1,135 +1,160 @@
-// SPDX-License-Identifier: unlicensed
+// SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract WaifuRewarder is Ownable, ReentrancyGuard {
-    IERC1155 public nftToken;
+contract WaifuRewarder is ERC1155Holder, ReentrancyGuard {
+    IERC1155 public waifuSeasonal;
     IERC20 public rewardToken;
+    uint256 public totalReward;
+    uint256 public rewardDuration;
+    uint256 public startTime;
+    uint256 public endTime;
+    uint256[] public rewardWeights;
 
-    uint256[] public rewardRates;
-    uint256 public totalDistributedRewards;
-    uint256 public totalcount;
-    address[] public stakers;
+    mapping(address => mapping(uint256 => uint256)) private _lastClaim;
+    mapping(address => mapping(uint256 => uint256)) private _userRewards;
+    mapping(address => mapping(uint256 => uint256)) private _stakedBalances;
+    mapping(uint256 => uint256) public totalStakedTokens; // added to track the total number of staked tokens for each tokenId
 
-    mapping(address => mapping(uint256 => uint256)) public stakedTokens;
-    mapping(address => uint256) public pendingRewards;
-    mapping(address => mapping(uint256 => uint256)) public claimedRewards;
-    mapping(uint256 => uint256) public stakedTokensTotal;
-    mapping(address => mapping(uint256 => uint256)) public lastRewardUpdate;
-
-    event TokensStaked(address indexed user, uint256 tokenId, uint256 amount);
-    event TokensUnstaked(address indexed user, uint256 tokenId, uint256 amount);
-    event RewardClaimed(
+    event Staked(address indexed user, uint256 indexed tokenId, uint256 amount);
+    event Unstaked(
         address indexed user,
         uint256 indexed tokenId,
         uint256 amount
     );
+    event RewardClaimed(
+        address indexed user,
+        uint256 indexed tokenId,
+        uint256 reward
+    );
 
     constructor(
-        IERC1155 _nftToken,
+        IERC1155 _waifuSeasonal,
         IERC20 _rewardToken,
-        uint256[] memory _rewardRates
+        uint256 _totalReward,
+        uint256 _rewardDuration,
+        uint256[] memory _rewardWeights
     ) {
-        nftToken = _nftToken;
+        // address of seasonal Waifu season
+        waifuSeasonal = _waifuSeasonal;
+        // address of $ISEKAI
         rewardToken = _rewardToken;
-        rewardRates = _rewardRates;
+        // total number of tokens at beginning of season
+        totalReward = _totalReward;
+        // define this in days
+        // rewardDuration = 69 days
+        rewardDuration = _rewardDuration;
+        startTime = block.timestamp;
+        endTime = startTime + _rewardDuration;
+        // this is an array with 24 entries, however many season waifus we have
+        // and their corresponding weights
+        // for erc721 unique waifus, we will assume tokenIds are the last two
+        // 1500, 3000 represent the two unique waifus
+        // [1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 20, 20, 20, 20, 100, 100, 100, 1500, 1500, 1500, 3000]
+        // this way rewardWeights[tokenId] will match to it's weight
+        rewardWeights = _rewardWeights;
     }
 
-    modifier onlyRewardToken() {
+    function stake(uint256 tokenId, uint256 amount) external nonReentrant {
+        require(block.timestamp < endTime, "Staking period has ended");
         require(
-            msg.sender == address(rewardToken),
-            "Caller is not the reward token"
+            waifuSeasonal.balanceOf(msg.sender, tokenId) >= amount,
+            "Insufficient balance to stake"
         );
-        _;
-    }
-
-    function totalStakedTokens() public view returns (uint256) {
-        return totalcount;
-    }
-
-    function stakeTokens(uint256 tokenId, uint256 amount)
-        external
-        nonReentrant
-    {
-        require(amount > 0, "Cannot stake zero tokens");
-        require(tokenId < rewardRates.length, "Invalid token ID");
+        require(amount > 0, "Amount should be greater than 0");
         claimReward(tokenId);
-        nftToken.safeTransferFrom(
+        waifuSeasonal.safeTransferFrom(
             msg.sender,
             address(this),
             tokenId,
             amount,
             ""
         );
-        stakedTokens[msg.sender][tokenId] += amount;
-        stakers.push(msg.sender);
-        stakedTokensTotal[tokenId] += amount;
-        totalcount += amount;
-
-        emit TokensStaked(msg.sender, tokenId, amount);
+        totalStakedTokens[tokenId] += amount; // update the totalStakedTokens mapping
+        _stakedBalances[msg.sender][tokenId] += amount; // map [user][tokenid] to amount
+        _updateRewards(msg.sender, tokenId);
+        emit Staked(msg.sender, tokenId, amount);
     }
 
-    function unstakeTokens(uint256 tokenId, uint256 amount)
-        external
-        nonReentrant
-    {
-        require(amount > 0, "Cannot unstake zero tokens");
-        require(tokenId < rewardRates.length, "Invalid token ID");
+    function unstake(uint256 tokenId, uint256 amount) external nonReentrant {
         require(
-            stakedTokens[msg.sender][tokenId] >= amount,
-            "Not enough staked tokens"
+            waifuSeasonal.balanceOf(address(this), tokenId) >= amount,
+            "Not enough tokens staked"
         );
         claimReward(tokenId);
-        nftToken.safeTransferFrom(
+        require(amount > 0, "Amount should be greater than 0");
+        _updateRewards(msg.sender, tokenId);
+        waifuSeasonal.safeTransferFrom(
             address(this),
             msg.sender,
             tokenId,
             amount,
             ""
         );
-
-        stakedTokens[msg.sender][tokenId] -= amount;
-        stakedTokensTotal[tokenId] -= amount;
-        totalcount -= amount;
-
-        emit TokensUnstaked(msg.sender, tokenId, amount);
-    }
-
-    function distributeReward(uint256 rewardAmount) external onlyRewardToken {
-        totalDistributedRewards += rewardAmount;
+        _stakedBalances[msg.sender][tokenId] -= amount;
+        // shouldnt this call _updateRewards?
+        totalStakedTokens[tokenId] -= amount;
+        emit Unstaked(msg.sender, tokenId, amount);
     }
 
     function claimReward(uint256 tokenId) public nonReentrant {
-        require(
-            stakedTokens[msg.sender][tokenId] > 0,
-            "Not staking this token"
+        _updateRewards(msg.sender, tokenId);
+        uint256 reward = _userRewards[msg.sender][tokenId];
+        require(reward > 0, "No rewards available");
+        _userRewards[msg.sender][tokenId] = 0;
+        rewardToken.transfer(msg.sender, reward);
+        emit RewardClaimed(msg.sender, tokenId, reward);
+    }
+
+    function _updateRewards(address user, uint256 tokenId) private {
+        uint256 accumulatedRewardPerToken = _getAccumulatedRewardPerToken(
+            tokenId
         );
+        uint256 userStakedBalance = _stakedBalances[user][tokenId];
+        uint256 newReward = (accumulatedRewardPerToken -
+            _lastClaim[user][tokenId]) *
+            userStakedBalance *
+            rewardWeights[tokenId];
+        _userRewards[user][tokenId] += newReward;
+        _lastClaim[user][tokenId] = accumulatedRewardPerToken;
+    }
 
-        uint256 stakedTokenAmount = stakedTokens[msg.sender][tokenId];
-        uint256 rewardRate = rewardRates[tokenId];
-        uint256 totalTokens = totalStakedTokens();
-        uint256 totalReward = rewardToken.balanceOf(address(this));
+    function viewClaimableReward(address user, uint256 tokenId)
+        external
+        view
+        returns (uint256)
+    {
+        uint256 accumulatedRewardPerToken = _getAccumulatedRewardPerToken(
+            tokenId
+        );
+        uint256 userStakedBalance = _stakedBalances[user][tokenId];
+        uint256 newReward = (accumulatedRewardPerToken -
+            _lastClaim[user][tokenId]) *
+            userStakedBalance *
+            rewardWeights[tokenId];
+        uint256 claimableReward = _userRewards[user][tokenId] + newReward;
+        return claimableReward;
+    }
 
-        uint256 lastUpdate = lastRewardUpdate[msg.sender][tokenId];
-        uint256 timeElapsed = block.timestamp - lastUpdate;
-        uint256 accumulatedReward = (totalReward *
+    function _getAccumulatedRewardPerToken(uint256 tokenId)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 timeElapsed = block.timestamp > endTime
+            ? endTime - startTime
+            : block.timestamp - startTime;
+        uint256 weightedTotalStaked = 0;
+        for (uint256 i = 0; i < rewardWeights.length; i++) {
+            weightedTotalStaked += totalStakedTokens[i] * rewardWeights[i];
+        }
+        uint256 accumulatedRewardPerToken = (totalReward *
             timeElapsed *
-            stakedTokenAmount *
-            rewardRate) / (totalTokens * 1 days);
-
-        uint256 pendingReward = accumulatedReward -
-            claimedRewards[msg.sender][tokenId];
-
-        require(pendingReward > 0, "No pending reward");
-
-        claimedRewards[msg.sender][tokenId] = accumulatedReward;
-        lastRewardUpdate[msg.sender][tokenId] = block.timestamp;
-        rewardToken.transfer(msg.sender, pendingReward);
-
-        emit RewardClaimed(msg.sender, tokenId, pendingReward);
+            rewardWeights[tokenId]) / (rewardDuration * weightedTotalStaked);
+        return accumulatedRewardPerToken;
     }
 }
