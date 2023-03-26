1a43da3a074426ba8b0f1b19b6068079eb9564f4
dniminenn <85250163+dniminenn@users.noreply.github.com>
2023-03-26 17:07:59 -0300

    alright, all of our ABIs are here

---
+++ website/html/js/abi/lprewarder.js
@@ -0,0 +1,287 @@
+const lprewarder_addr = '0x1234...'; // farming LPs for crystals
+const lprewarder_abi = [
+    [
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "_lpToken",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "_crystals",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_rewardPerBlock",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "previousOwner",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "newOwner",
+                    "type": "address"
+                }
+            ],
+            "name": "OwnershipTransferred",
+            "type": "event"
+        },
+        {
+            "inputs": [],
+            "name": "claimReward",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "crystals",
+            "outputs": [
+                {
+                    "internalType": "contract ICrystalsToken",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "userAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "getPendingReward",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "userAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "getStakedAmountForUser",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "lastUpdateBlock",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "lpToken",
+            "outputs": [
+                {
+                    "internalType": "contract IERC20",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "renounceOwnership",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "rewardAccumulationRate",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "rewardPerBlock",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_newRewardPerBlock",
+                    "type": "uint256"
+                }
+            ],
+            "name": "setRewardPerBlock",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "stake",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "totalStaked",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "newOwner",
+                    "type": "address"
+                }
+            ],
+            "name": "transferOwnership",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "name": "userInfo",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "staked",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "lastActionBlock",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "rewardDebt",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "withdraw",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address payable",
+                    "name": "_to",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "withdrawOwner",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+]
--- website/html/js/polygonconnect.js
+++ website/html/js/polygonconnect.js
@@ -5,10 +5,6 @@ const connectButton = document.getElementById('connect-button');
 // Initialize the Web3 instance
 const web3 = new Web3(window.ethereum);
 
-// Create the contract instance
-// Replace ABI and season number (maybe load from dropdown?)
-const seasonnft = new web3.eth.Contract(season0_abi, season0_addr);
-
 // Check if the user is connected to Polygon
 async function checkPolygonConnection() {
   if (typeof window.ethereum !== 'undefined') {
@@ -40,28 +36,3 @@ checkPolygonConnection();
 
 // Listen for the chain change event
 window.ethereum.on('chainChanged', checkPolygonConnection);
-
-// Request mint with crystals
-async function requestMintWithCrystals(amount) {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  await seasonnft.methods.requestMintCrystals(amount).send({ from: account });
-}
-
-// Add event listener for mint with crystals
-const mintWithCrystalsButton = document.getElementById('mint-with-crystals-button');
-mintWithCrystalsButton.addEventListener('click', async () => {
-  const amount = 1; // Replace with the desired amount
-  await requestMintWithCrystals(amount);
-});
-
-// Listen for MintProcessed events
-seasonnft.events.MintProcessed({}, (error, event) => {
-  if (error) {
-    console.error('Error in event:', error);
-  } else {
-    const { user, tokenIds, nonce } = event.returnValues;
-    console.log(`Mint processed for user ${user} with tokenIds ${tokenIds} and nonce ${nonce}`);
-    // Update the UI based on the new tokens
-  }
-});
